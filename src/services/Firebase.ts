a92f787a3dc88842d7b990d561f0a0d511ea00a4
takusamar <takusamar@gmail.com>
2024-07-20 15:37:18 +0900

    history書き込み処理

--- src/services/Firebase.ts
+++ src/services/Firebase.ts
@@ -1,11 +1,11 @@
-// Import the functions you need from the SDKs you need
 import { FirebaseOptions, getApps, initializeApp } from "firebase/app";
-import { initializeFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import {
+  addDoc,
+  collection,
+  getFirestore,
+  initializeFirestore,
+} from "firebase/firestore";
+import { History, historyConverter } from "../models/History";
 
 function _initFirebase(config: FirebaseOptions) {
   // Initialize Firebase
@@ -24,5 +24,23 @@ export async function initFirebase(name?: string) {
       const config = await response.json();
       _initFirebase(config);
     }
+  }
+}
+
+export async function createHistory(
+  userId: string,
+  data: Partial<Omit<History, "id" | "createdAt" | "updatedAt">>
+) {
+  try {
+    const db = getFirestore();
+    const col = collection(db, "users", userId, "histories").withConverter(
+      historyConverter
+    );
+    return await addDoc(col, data);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+    throw error;
   }
 }
